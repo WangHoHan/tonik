296fedb6533195e1e7f3d197c275331b503c457b
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2022-04-14 16:03:44 +0200

    Fire API request when user stops typing

--- src/pages/root/Navigation/Navigation.tsx
+++ src/pages/root/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import {HeaderStyled, NavStyled, SearchBar} from './Navigation.styled';
-import React, {Dispatch, useState} from 'react';
+import React, {Dispatch, useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux';
 import {getRepositories} from '../../../store/data/repositories/actions';
 
@@ -8,19 +8,20 @@ const Navigation: React.FC = (): JSX.Element => {
 
     const [input, setInput] = useState<string>('');
 
-    const handleSearchBarOnInput = (e: any): void => {
-        const word: string = e.target.value;
-        setInput(word);
-        if (word) {
-            dispatch(getRepositories({
-                q: word,
-                sort: 'stars',
-                order: 'asc',
-                perPage: 30,
-                page: 1
-            }));
-        }
-    };
+    useEffect((): () => void => {
+        const delayDebounceFn: NodeJS.Timeout = setTimeout((): any => {
+            if (input) {
+                dispatch(getRepositories({
+                    q: input,
+                    sort: 'stars',
+                    order: 'asc',
+                    perPage: 30,
+                    page: 1
+                }));
+            }
+        }, 400);
+        return (): void => clearTimeout(delayDebounceFn);
+    }, [dispatch, input]);
 
     return (
         <HeaderStyled>
@@ -29,7 +30,7 @@ const Navigation: React.FC = (): JSX.Element => {
                            placeholder='Search for GitHub repositories'
                            onFocus={(e: any) => e.target.placeholder = ''}
                            onBlur={(e: any) => e.target.placeholder = 'Search for GitHub repositories'}
-                           onInput={handleSearchBarOnInput}
+                           onInput={(e: any) => setInput(e.target.value)}
                            value={input}/>
             </NavStyled>
         </HeaderStyled>
