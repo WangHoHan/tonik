ecd2ddadf0789cf0d261e298408b9ea0802f1874
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2022-04-18 13:35:21 +0200

    Add PageNotFound tests

---
+++ src/pages/common/PageNotFound/__tests__/PageNotFound.test.tsx
@@ -0,0 +1,40 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import React, {useEffect} from "react";
+import {Provider} from 'react-redux';
+import {store} from '../../../../store/store';
+import {BrowserRouter} from 'react-router-dom';
+import Navigation from "../../../root/Navigation/Navigation";
+import HomePage from '../../../HomePage/HomePage';
+import PageNotFound from '../PageNotFound';
+
+const MockPageNotFound: React.FC = (): JSX.Element => {
+
+    return (
+        <Provider store={store}>
+            <Navigation/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/home' element={<HomePage/>}/>
+                    <Route path='*' element={<PageNotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    );
+};
+
+describe('PageNotFound', (): void => {
+    it('should render PageNotFound component properly', (): void => {
+        render(<MockPageNotFound/>);
+        const button: HTMLElement = screen.getByText(/back to home page/i);
+        expect(button).toBeInTheDocument();
+    });
+
+    it('should move to HomePage on Back to Home Page click', (): void => {
+        render(<MockPageNotFound/>);
+        const button: HTMLElement = screen.getByText(/back to home page/i);
+        fireEvent.click(button);
+        const loading: HTMLElement = screen.getByText(/loading/i);
+        expect(loading).toBeInTheDocument();
+    });
+});
